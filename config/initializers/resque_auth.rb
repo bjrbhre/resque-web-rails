226f9b4e897980abece993cb70432e02132d12da
Pierre Beauhaire <bjr@bhre.me>
2015-02-22 21:42:18 +0100

    resque_auth based on ENV variables

--- config/initializers/resque_auth.rb
+++ config/initializers/resque_auth.rb
@@ -1,3 +1,12 @@
 Resque::Server.use(Rack::Auth::Basic) do |user, password|
-  password == "secret"
+  # return true if Rails.env.development?
+
+  env_password = ENV['RESQUE_PASSWORD']
+  env_user = ENV['RESQUE_USER']
+
+  # check password and user
+  env_password.present? &&
+    env_password == password &&
+    env_user.present? &&
+    env_user == user
 end
