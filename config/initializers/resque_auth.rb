fc99851142e595d71878a3e45e10fb11a0b62742
Pierre Beauhaire <bjr@bhre.me>
2015-09-18 16:29:41 +0200

    Check if basic auth is required

--- config/initializers/resque_auth.rb
+++ config/initializers/resque_auth.rb
@@ -1,12 +1,14 @@
-Resque::Server.use(Rack::Auth::Basic) do |user, password|
-  # return true if Rails.env.development?
+if ENV['RESQUE_USER']
+  Resque::Server.use(Rack::Auth::Basic) do |user, password|
+    # return true if Rails.env.development?
 
-  env_password = ENV['RESQUE_PASSWORD']
-  env_user = ENV['RESQUE_USER']
+    env_password = ENV['RESQUE_PASSWORD']
+    env_user = ENV['RESQUE_USER']
 
-  # check password and user
-  env_password.present? &&
-    env_password == password &&
-    env_user.present? &&
-    env_user == user
+    # check password and user
+    env_password.present? &&
+      env_password == password &&
+      env_user.present? &&
+      env_user == user
+  end
 end
